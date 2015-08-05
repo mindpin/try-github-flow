1e1b65434740c9838c0a5e27827440c1c420aa33
libochao <522427432@qq.com>
2015-08-05 17:34:44 +0800

    page confirm

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "/zjb" => "jian_bin#index"
 
   get "/szq" => "szq_shan#index"
+  get "/sessions/new" => "sessions#new"
+  post"/sessions/create"=> "sessions#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/models/lbc_order_spec.rb
+++ spec/models/lbc_order_spec.rb
@@ -24,5 +24,24 @@ RSpec.describe Order, type: :model do
     end
 
   end
+
+
+# 测试页面
+    describe "the signin process", :type => :feature do
+  # before :each do
+  #   User.make(:email => 'user@example.com', :password => 'password')
+  # end
+
+  it "signs me in" do
+    visit '/sessions/new'
+    within("#session") do
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Success'
+  end
+end
+
   
 end
