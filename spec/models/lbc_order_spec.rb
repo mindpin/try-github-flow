f2fb4ec25ce42f9eff9d57e1e946cd7e7283d3f1
libochao <522427432@qq.com>
2015-08-05 12:03:49 +0800

    factories is used

--- spec/models/lbc_order_spec.rb
+++ spec/models/lbc_order_spec.rb
@@ -8,14 +8,18 @@ RSpec.describe Order, type: :model do
   describe "amount"do
 
     it "是vip九折 "  do
-      user = User.new(:is_vip =>true)
-      order=Order.new(:user=>user, :total=>1000)
+      user =create(:user_is_vip)
+      # (:is_vip =>true)
+      # order=Order.new(:user=>user, :total=>1000)
+      order=create(:order_aa,:user=>user)
       expect(order.amount).to eq(900)
     end
 
     it "不是vip全价 "  do
-      user = User.new(:is_vip =>false)
-      order=Order.new(:user=>user, :total=>1000)
+      # user = User.new(:is_vip =>false)
+      # order=Order.new(:user=>user, :total=>1000)
+      user =create(:user_is_not_vip)
+      order=create(:order_aa,:user=>user)
       expect(order.amount).to eq(1000)
     end
 
